43340f74a5a43096d828d6622595fb59b5f4ea41
AnthonyFurman <71467649+AnthonyFurman@users.noreply.github.com>
2021-11-16 15:10:30 -0800

    fix import

--- Team-4-Code/src/main/java/frc/robot/RobotContainer.java
+++ Team-4-Code/src/main/java/frc/robot/RobotContainer.java
@@ -12,13 +12,12 @@ import edu.wpi.first.wpilibj.XboxController;
 import frc.robot.subsystems.DriveSubsystem;
 
 import frc.robot.subsystems.IntakeSubsystem;
+import frc.robot.subsystems.OuttakeSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj2.command.*;
-import frc.robot.Constants.DriveConstants;
-import frc.robot.Constants.IntakeConstants;
 import frc.robot.Constants.JoystickConstants;
 
 
